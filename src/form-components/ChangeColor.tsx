c95dc0f22f7445ec3cf77f546a76c556fde2d4ab
Austin Cory Bart <acbart@vt.edu>
2024-08-24 14:58:30 -0400

    This one too

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export function ChangeColor(): JSX.Element {
+export function ChangeColor(): React.JSX.Element {
     return (
         <div>
             <h3>Change Color</h3>
