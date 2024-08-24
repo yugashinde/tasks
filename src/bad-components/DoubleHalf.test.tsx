50a9c85dca9bc60f7dd875355f2c28c3988ca610
Austin Cory Bart <acbart@vt.edu>
2024-08-24 14:47:15 -0400

    Update for new react

--- src/bad-components/DoubleHalf.test.tsx
+++ src/bad-components/DoubleHalf.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { act } from "react";
 import { render, screen } from "@testing-library/react";
 import { DoubleHalf } from "./DoubleHalf";
 
@@ -6,50 +6,66 @@ describe("DoubleHalf Component tests", () => {
     beforeEach(() => {
         render(<DoubleHalf />);
     });
-    test("The DoubleHalf value is initially 10", () => {
+    test("(2 pts) The DoubleHalf value is initially 10", () => {
         expect(screen.getByText("10")).toBeInTheDocument();
         expect(screen.queryByText("20")).not.toBeInTheDocument();
         expect(screen.queryByText("5")).not.toBeInTheDocument();
     });
-    test("There are Double and Halve buttons", () => {
+    test("(2 pts) There are Double and Halve buttons", () => {
         expect(
-            screen.getByRole("button", { name: /Double/i })
+            screen.getByRole("button", { name: /Double/i }),
         ).toBeInTheDocument();
         expect(
-            screen.getByRole("button", { name: /Halve/i })
+            screen.getByRole("button", { name: /Halve/i }),
         ).toBeInTheDocument();
     });
-    test("You can double the number.", () => {
+    test("(2 pts) You can double the number.", async () => {
         const double = screen.getByRole("button", { name: /Double/i });
-        double.click();
+        await act(async () => {
+            double.click();
+        });
         expect(screen.getByText("20")).toBeInTheDocument();
         expect(screen.queryByText("10")).not.toBeInTheDocument();
     });
-    test("You can halve the number.", () => {
+    test("(2 pts) You can halve the number.", async () => {
         const halve = screen.getByRole("button", { name: /Halve/i });
-        halve.click();
+        await act(async () => {
+            halve.click();
+        });
         expect(screen.getByText("5")).toBeInTheDocument();
         expect(screen.queryByText("10")).not.toBeInTheDocument();
     });
-    test("You can double AND halve the number.", () => {
+    test("(2 pts) You can double AND halve the number.", async () => {
         const double = screen.getByRole("button", { name: /Double/i });
         const halve = screen.getByRole("button", { name: /Halve/i });
-        double.click();
+        await act(async () => {
+            double.click();
+        });
         expect(screen.getByText("20")).toBeInTheDocument();
         expect(screen.queryByText("10")).not.toBeInTheDocument();
-        double.click();
+        await act(async () => {
+            double.click();
+        });
         expect(screen.getByText("40")).toBeInTheDocument();
         expect(screen.queryByText("20")).not.toBeInTheDocument();
-        halve.click();
+        await act(async () => {
+            halve.click();
+        });
         expect(screen.getByText("20")).toBeInTheDocument();
         expect(screen.queryByText("10")).not.toBeInTheDocument();
-        halve.click();
+        await act(async () => {
+            halve.click();
+        });
         expect(screen.getByText("10")).toBeInTheDocument();
         expect(screen.queryByText("20")).not.toBeInTheDocument();
-        halve.click();
+        await act(async () => {
+            halve.click();
+        });
         expect(screen.getByText("5")).toBeInTheDocument();
         expect(screen.queryByText("10")).not.toBeInTheDocument();
-        halve.click();
+        await act(async () => {
+            halve.click();
+        });
         expect(screen.getByText("2.5")).toBeInTheDocument();
         expect(screen.queryByText("5")).not.toBeInTheDocument();
     });
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+function Doubler(): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(2 * dhValue);
+            }}
+        >
+            Double
+        </Button>
+    );
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Halver(): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(0.5 * dhValue);
+            }}
+        >
+            Halve
+        </Button>
+    );
 }
 
-export function DoubleHalf(): JSX.Element {
+export function DoubleHalf(): React.JSX.Element {
     return (
         <div>
             <h3>Double Half</h3>
